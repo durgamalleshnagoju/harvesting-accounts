ddeaefa9a6ae7e4a3bab0df9ff803931e74f0df4
mnagoju <mnagoju@cotractor.ea.com>
2024-12-25 21:53:43 +0530

    Initial commit

---
+++ src/main/java/com/harvesting_accounts/HarvestingAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.harvesting_accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarvestingAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HarvestingAccountsApplication.class, args);
+	}
+
+}
